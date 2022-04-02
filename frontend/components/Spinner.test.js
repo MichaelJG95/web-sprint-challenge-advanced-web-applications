f581ae2ba60a1147b19cc074cae373b126f03930
Michael Gutierrez <michaelgutierrezj@gmail.com>
2022-04-01 21:15:33 -0500

    added tests for spinner

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 import Spinner from './Spinner'
 
 // Import the Spinner component into this file and test
@@ -10,4 +11,16 @@ test('sanity', () => {
 
 test('render Spinner without errors', () => {
   render(<Spinner />)
+})
+
+test('renders please wait when true is passed to on prop', () => {
+  render(<Spinner on={true} />)
+  const spinner = screen.queryByText('Please wait...')
+  expect(spinner).toBeInTheDocument()
+})
+
+test('does not renders please wait when false is passed to on prop', () => {
+  render(<Spinner on={false} />)
+  const spinner = screen.queryByText('Please wait...')
+  expect(spinner).not.toBeInTheDocument
 })
