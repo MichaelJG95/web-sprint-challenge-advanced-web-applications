96cefe9ea3d2614c36a18dab8837545e1a7a312b
Michael Gutierrez <michaelgutierrezj@gmail.com>
2022-04-01 19:01:09 -0500

    commit 2

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { NavLink, Routes, Route, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import axiosWithAuth from '../axios'
 import Articles from './Articles'
 import LoginForm from './LoginForm'
 import Message from './Message'
@@ -18,8 +20,8 @@ export default function App() {
 
   // ✨ Research `useNavigate` in React Router v.6
   const navigate = useNavigate()
-  const redirectToLogin = () => { /* ✨ implement */ }
-  const redirectToArticles = () => { /* ✨ implement */ }
+  const redirectToLogin = () => { /* ✨ implement */ navigate('/') }
+  const redirectToArticles = () => { /* ✨ implement */ navigate('/articles') }
 
   const logout = () => {
     // ✨ implement 
@@ -27,6 +29,9 @@ export default function App() {
     // and a message saying "Goodbye!" should be set in its proper state.
     // In any case, we should redirect the browser back to the login screen,
     // using the helper above.
+    window.localStorage.removeItem('token')
+    setMessage('Goodbye!')
+    redirectToLogin()
   }
 
   const login = ({ username, password }) => {
@@ -36,6 +41,18 @@ export default function App() {
     // On success, we should set the token to local storage in a 'token' key,
     // put the server success message in its proper state, and redirect
     // to the Articles screen. Don't forget to turn off the spinner!
+    setMessage('')
+    setSpinnerOn(true)
+    axios.post(loginUrl, { username, password })
+      .then(res => {
+        window.localStorage.setItem('token', res.data.token)
+        setMessage(res.data.message)
+        redirectToArticles()
+        setSpinnerOn(false)
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 
   const getArticles = () => {
@@ -47,6 +64,19 @@ export default function App() {
     // If something goes wrong, check the status of the response:
     // if it's a 401 the token might have gone bad, and we should redirect to login.
     // Don't forget to turn off the spinner!
+    setMessage('')
+    setSpinnerOn(true)
+    axiosWithAuth().get('/articles')
+      .then(res => {
+        setArticles(res.data.articles)
+        setMessage(res.data.message)
+        setSpinnerOn(false)
+      })
+      .catch(err => {
+        console.log(err)
+        setSpinnerOn(false)
+        redirectToLogin()
+      })
   }
 
   const postArticle = article => {
@@ -54,22 +84,67 @@ export default function App() {
     // The flow is very similar to the `getArticles` function.
     // You'll know what to do! Use log statements or breakpoints
     // to inspect the response from the server.
+    setMessage('')
+    setSpinnerOn(true)
+    axiosWithAuth().post('/articles', article)
+      .then(res => {
+        setArticles([...articles, res.data.article])
+        setMessage(res.data.message)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+      .finally(() => {
+        setSpinnerOn(false)
+      })
   }
 
   const updateArticle = ({ article_id, article }) => {
     // ✨ implement
     // You got this!
+    setMessage('')
+    setSpinnerOn(true)
+    axiosWithAuth().put(`/articles/${article_id}`, article)
+      .then(res => {
+        setArticles(articles.map(art => {
+          return art.article_id === article_id
+            ? res.data.article
+            : art
+        }))
+        setMessage(res.data.message)
+        setCurrentArticleId(null)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+      .finally(() => {
+        setSpinnerOn(false)
+      })
   }
 
   const deleteArticle = article_id => {
     // ✨ implement
+    setMessage('')
+    setSpinnerOn(true)
+    axiosWithAuth().delete(`/articles/${article_id}`)
+      .then(res => {
+        console.log(res)
+        setMessage(res.data.message)
+        setArticles(articles.filter(article => {
+          return article.article_id !== article_id
+        }))
+        setSpinnerOn(false)
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 
   return (
     // ✨ fix the JSX: `Spinner`, `Message`, `LoginForm`, `ArticleForm` and `Articles` expect props ❗
     <React.StrictMode>
-      <Spinner />
-      <Message />
+      <Spinner on={spinnerOn} />
+      <Message message={message} />
       <button id="logout" onClick={logout}>Logout from app</button>
       <div id="wrapper" style={{ opacity: spinnerOn ? "0.25" : "1" }}> {/* <-- do not change this line */}
         <h1>Advanced Web Applications</h1>
@@ -78,11 +153,23 @@ export default function App() {
           <NavLink id="articlesScreen" to="/articles">Articles</NavLink>
         </nav>
         <Routes>
-          <Route path="/" element={<LoginForm />} />
+          <Route path="/" element={<LoginForm login={login} />} />
           <Route path="articles" element={
             <>
-              <ArticleForm />
-              <Articles />
+              <ArticleForm 
+                postArticle={postArticle}
+                updateArticle={updateArticle}
+                currentArticle={articles.find(art => art.article_id === currentArticleId)} 
+                currentArticleId={currentArticleId}
+                setCurrentArticleId={setCurrentArticleId}
+              />
+              <Articles 
+                articles={articles} 
+                currentArticleId={currentArticleId}
+                setCurrentArticleId={setCurrentArticleId}
+                getArticles={getArticles} 
+                deleteArticle={deleteArticle}
+              />
             </>
           } />
         </Routes>
--- frontend/components/ArticleForm.js
+++ frontend/components/ArticleForm.js
@@ -6,13 +6,15 @@ const initialFormValues = { title: '', text: '', topic: '' }
 export default function ArticleForm(props) {
   const [values, setValues] = useState(initialFormValues)
   // ✨ where are my props? Destructure them here
+  const { postArticle, updateArticle, currentArticle, currentArticleId, setCurrentArticleId } = props
 
   useEffect(() => {
     // ✨ implement
     // Every time the `currentArticle` prop changes, we should check it for truthiness:
     // if it's truthy, we should set its title, text and topic into the corresponding
     // values of the form. If it's not, we should reset the form back to initial values.
-  })
+    setValues(currentArticle || initialFormValues)
+  }, [currentArticle])
 
   const onChange = evt => {
     const { id, value } = evt.target
@@ -24,11 +26,19 @@ export default function ArticleForm(props) {
     // ✨ implement
     // We must submit a new post or update an existing one,
     // depending on the truthyness of the `currentArticle` prop.
+    currentArticle ? updateArticle({ article_id: currentArticleId, article: values}) : postArticle(values)
+    setValues(initialFormValues)
   }
 
   const isDisabled = () => {
     // ✨ implement
     // Make sure the inputs have some values
+    const { title, text, topic } = values
+    if (title.trim().length >= 1 && text.trim().length >= 1 && topic.trim().length >= 1){
+      return false
+    } else {
+      return true
+    }
   }
 
   return (
@@ -58,7 +68,7 @@ export default function ArticleForm(props) {
       </select>
       <div className="button-group">
         <button disabled={isDisabled()} id="submitArticle">Submit</button>
-        <button onClick={Function.prototype}>Cancel edit</button>
+        <button hidden={!currentArticleId} onClick={() => setCurrentArticleId(null)}>Cancel edit</button>
       </div>
     </form>
   )
